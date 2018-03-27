1c836ed1e2080bde1d3b32b94c61302e2b70adf9
William Colgan <william@Williams-MacBook-Pro-3.local>
2018-03-27 11:00:54 -0400

    More stuff

--- sat_finder.py
+++ sat_finder.py
@@ -6,16 +6,32 @@ from file_loading import *
 from utils import *
 from classify import *
 import numpy as np
+import matplotlib.pyplot as plt
 
+name = "CHR1_11321M"
 #load and process files
 subfamilies = load_subfamilies("subfamilies.txt")
 mers = load_mers("24mers.txt")
 probs = get_probs(mers,subfamilies)
-reads = load_reads("reads.fasta")
+reads = load_reads(name+".sat")
 lookup = subfamilies.keys()
 
 #classify each read
+num = np.zeros(14)
+prob_list = []
+for i in range(14):
+    prob_list += [[]]
+
 for read in reads:
     pred = classify(read.seq,probs)
-    subfamily = lookup[np.argmax(pred)]
-    print(read.description+" "+subfamily)
+    i = np.argmax(pred)
+    num[i] += 1
+    prob_list[i] += [pred[i]]
+
+for i in range(14):
+    print(lookup[i]+ ": " +str(num[i]))
+    plt.figure(i)
+    prob_list[i]
+    plt.hist(np.nan_to_num(prob_list[i]),100)
+    plt.title("Confidence "+name+" "+lookup[i])
+    plt.show()
